a2dee59d5bc4bd07d1066976f221b621244c4639
{KAZUMA} <{qazu38@gmail.com}>
2025-01-20 15:51:13 +0900

    APPにログインありか分岐を実装

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,37 @@ import Chat from './pages/chat/Chat';
 import CourseList from './pages/course/CourseList';
 import Settings from './pages/settings/Settings';
 import Login from './pages/login/Login';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    // 初回レンダリング時にローカルストレージをチェック
+    const storedUserId = localStorage.getItem('userId');
+    if (storedUserId) {
+      setUserId(storedUserId);
+    }
+  }, []);
+
   return (
-    <Login />
-    // <Router>
-    //   <Layout>
-    //     <Routes>
-    //       <Route path="/" element={<Login />} />
-    //       <Route path="/chat" element={<Chat />} />
-    //       <Route path="/course" element={<CourseList />} />
-    //       <Route path="/settings" element={<Settings />} />
-    //     </Routes>
-    //   </Layout>
-    // </Router>
+    <div>
+      {userId ? (
+        <Router>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/chat" element={<Chat />} />
+              <Route path="/course" element={<CourseList />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </Layout>
+        </Router>
+      ):(
+        <Login />
+      )}
+    </div>
   );
 };
 
